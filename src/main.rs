79351e2f5a5b35f3f7ab1a9315150de662d564ec
Matt Lese <matthewjlese@gmail.com>
2025-12-29 11:15:43 -0800

    parses address arg into InputType enum variants

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
 use anyhow::Result;
 use clap::Parser;
+use url::Url;
 
 #[derive(Parser)]
 struct Cli {
     address: String,
     #[command(flatten)]
     verbosity: clap_verbosity_flag::Verbosity,
+}
+
+enum InputType {
+    FullUrl(Url),
+    FuzzyPattern(Vec<String>),
+}
+
+fn classify_input(address: &str) -> InputType {
+    if let Ok(url) = Url::parse(address) {
+        return InputType::FullUrl(url);
+    }
+
+    let with_scheme = format!("https://{}", address);
+    if let Ok(url) = Url::parse(&with_scheme) {
+        return InputType::FullUrl(url);
+    }
+
+    InputType::FuzzyPattern(address.split('/').map(String::from).collect())
 }
 
 fn main() -> Result<()> {
