d53a4c23cb212e8c7b97e669a48421bed94ea592
Matt Lese <matthewjlese@gmail.com>
2025-12-29 11:29:29 -0800

    adds note

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,8 @@ fn classify_input(address: &str) -> InputType {
 
     let with_scheme = format!("https://{}", address);
     if let Ok(url) = Url::parse(&with_scheme) {
+        // XXX: for now, we're assuming that, if the user didn't input a scheme, we can differentiate between a fuzzy pattern
+        //   and a domain that just needs https prepended by the presence of a '.'
         if url.host_str().map_or(false, |h| h.contains('.')) {
             return InputType::FullUrl(url);
         }
